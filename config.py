36c3bc847935d267a655570f49e30dc23ea85478
WangZhanze <wangzhanze@huoban.ai>
2024-08-30 19:49:49 +0800

    proxy config can be None

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     token: str  # 你的访问令牌 https://docs.github.com/zh/rest/actions/workflows?apiVersion=2022-11-28
     default_workflow_name: str = "ApiDockerImagePusher"
-    http_proxy: str = None
-    https_proxy: str = None
+    http_proxy: str | None = None
+    https_proxy: str | None = None
 
     class Config:
         env_file = ".env"
