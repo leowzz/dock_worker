00c6b580f4600fdd32d32a0043aabac4dd84da60
WangZhanze <wangzhanze@huoban.ai>
2024-08-30 15:05:02 +0800

    def get info func, add proxy

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    token: str
+    token: str  # 你的访问令牌 https://docs.github.com/zh/rest/actions/workflows?apiVersion=2022-11-28
+    http_proxy: str = None
+    https_proxy: str = None
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
--- server.py
+++ server.py
@@ -1,7 +1,27 @@
 import requests
 from config import settings
 
-url = "https://api.github.com/repos/leowzz/docker_image_pusher/repository_dispatch"
+github_repo_endpoint = "https://api.github.com/repos"
+# 代理服务器的 URL
+proxy = {
+    'http': settings.http_proxy,
+    'https': settings.http_proxy
+}
+
+
+def get_workflows(user, repo):
+    response = requests.get(
+        url=f"{github_repo_endpoint}/{user}/{repo}/actions/workflows",
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {settings.token}",
+            "X-GitHub-Api-Version": "2022-11-28"
+        },
+        proxies=proxy if settings.http_proxy else None
+    )
+    return response.json()
+
+
 url = "https://api.github.com/repos/leowzz/docker_image_pusher/actions/workflows"
 headers = {
     "Accept": "application/vnd.github+json",
@@ -16,11 +36,16 @@ data = {
     }
 }
 
+# python:3.10.14-slim-bullseye
+
+if __name__ == '__main__':
+    print(get_workflows("leowzz", "docker_image_pusher"))
+
 # response = requests.post(url, headers=headers, json=data)
-response = requests.get(url, headers=headers)
-print(response.status_code)
-print(response.text)
-if response.status_code == 204:
-    print("Request was successful.")
-else:
-    print(f"Request failed with status code: {response.status_code}")
+# response = requests.get(url, headers=headers)
+# print(response.status_code)
+# print(response.text)
+# if response.status_code == 204:
+#     print("Request was successful.")
+# else:
+#     print(f"Request failed with status code: {response.status_code}")
