2eafc42652e188d23d8fa664d50ad3f51d98ab9d
WangZhanze <wangzhanze@huoban.ai>
2024-08-30 16:31:43 +0800

    format

--- server.py
+++ server.py
@@ -4,109 +4,102 @@ from loguru import logger
 from pydantic import BaseModel
 from datetime import datetime
 
-github_api_endpoint = "https://api.github.com"
-# 代理服务器的 URL
-proxy = {"http": settings.http_proxy, "https": settings.http_proxy}
 
-
-class Workflow(BaseModel):
-    id: int
-    node_id: str
-    name: str
-    path: str
-    state: str
-    created_at: datetime
-    updated_at: datetime
-    url: str
-    html_url: str
-    badge_url: str
-
-
-class WorkflowsResponse(BaseModel):
-    total_count: int
-    workflows: list[Workflow]
-
-
-class WorkflowDetails(BaseModel):
-    id: int
-    node_id: str
-    name: str
-    path: str
-    state: str
-    created_at: datetime
-    updated_at: datetime
-    url: str
-    html_url: str
-    badge_url: str
-
-
-def get_workflows(owner, repo):
-    response = requests.get(
-        url=f"{github_api_endpoint}/repos/{owner}/{repo}/actions/workflows",
-        headers={
-            "Accept": "application/vnd.github+json",
-            "Authorization": f"Bearer {settings.token}",
-            "X-GitHub-Api-Version": "2022-11-28",
-        },
-        proxies=proxy if settings.http_proxy else None,
+class GitHubActionTrigger:
+    github_api_endpoint = "https://api.github.com"
+    proxy = (
+        {"http": settings.http_proxy, "https": settings.http_proxy}
+        if settings.http_proxy
+        else None
     )
-    resp_json = response.json()
-    res = WorkflowsResponse.model_validate(resp_json)
-    return res
 
-
-def get_workflow_info(owner, repo, workflow_id):
-    response = requests.get(
-        url=f"{github_api_endpoint}/repos/{owner}/{repo}/actions/workflows/{workflow_id}",
-        headers={
+    @property
+    def headers(self):
+        return {
             "Accept": "application/vnd.github+json",
             "Authorization": f"Bearer {settings.token}",
             "X-GitHub-Api-Version": "2022-11-28",
-        },
-        proxies=proxy if settings.http_proxy else None,
-    )
-    resp_json = response.json()
-    res = WorkflowDetails.model_validate(resp_json)
-    return res
+        }
 
+    class Workflow(BaseModel):
+        id: int
+        node_id: str
+        name: str
+        path: str
+        state: str
+        created_at: datetime
+        updated_at: datetime
+        url: str
+        html_url: str
+        badge_url: str
 
-def create_workflow_dispatch_event(owner, repo, workflow_id):
-    response = requests.post(
-        url=f"{github_api_endpoint}/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
-        headers={
-            "Accept": "application/vnd.github+json",
-            "Authorization": f"Bearer {settings.token}",
-            "X-GitHub-Api-Version": "2022-11-28",
-        },
-        proxies=proxy if settings.http_proxy else None,
-        json={
-            "ref": "main",
-            "inputs": {
-                "origin_image": "python:3.10.14-slim-bullseye",
-                "self_repo_image": "python_self:3.10.14-slim-bullseye"  # 私有仓库镜像, aliyun.com/your_space/{self_repo_image}
+    class WorkflowsResponse(BaseModel):
+        total_count: int
+        workflows: list["GitHubActionTrigger.Workflow"]
+
+    class WorkflowDetails(BaseModel):
+        id: int
+        node_id: str
+        name: str
+        path: str
+        state: str
+        created_at: datetime
+        updated_at: datetime
+        url: str
+        html_url: str
+        badge_url: str
+
+    class WorkflowTriggerArgs(BaseModel):
+        origin_image: str
+        self_repo_image: str  # 私有仓库镜像, aliyun.com/your_space/{self_repo_image}
+
+    def get_workflows(self, owner, repo):
+        response = requests.get(
+            url=f"{self.github_api_endpoint}/repos/{owner}/{repo}/actions/workflows",
+            headers=self.headers,
+            proxies=self.proxy,
+        )
+        resp_json = response.json()
+        res = self.WorkflowsResponse.model_validate(resp_json)
+        return res
+
+    def get_workflow_info(self, owner, repo, workflow_id):
+        response = requests.get(
+            url=f"{self.github_api_endpoint}/repos/{owner}/{repo}/actions/workflows/{workflow_id}",
+            headers=self.get_headers(),
+            proxies=self.proxy,
+        )
+        resp_json = response.json()
+        res = self.WorkflowDetails.model_validate(resp_json)
+        return res
+
+    def create_workflow_dispatch_event(self, owner, repo, workflow_id, ref="main",
+                                       trigger_args: WorkflowTriggerArgs = None):
+        if not trigger_args:
+            return None
+        response = requests.post(
+            url=f"{self.github_api_endpoint}/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+            headers=self.headers,
+            proxies=self.proxy,
+            json={
+                "ref": ref,
+                "inputs": trigger_args.model_dump(),
             },
-        },
-    )
-    return response.text
+        )
+        return response.text
 
-
-# python:3.10.14-slim-bullseye
 
 if __name__ == "__main__":
-    workflows = get_workflows("leowzz", "docker_image_pusher")
-    # logger.info(workflows)
+    action_trigger = GitHubActionTrigger()
+    workflows = action_trigger.get_workflows("leowzz", "docker_image_pusher")
     api_workflow_id = workflows.workflows[0].id
     logger.debug(f"{workflows.workflows[0]=}")
-    # api_workflow_info = get_workflow_info("leowzz", "docker_image_pusher", api_workflow_id)
-    # logger.info(api_workflow_info)
     logger.info(f"{api_workflow_id=}")
-    trigger_res = create_workflow_dispatch_event("leowzz", "docker_image_pusher", api_workflow_id)
+    trigger_args = action_trigger.WorkflowTriggerArgs(
+        origin_image="python:3.10.14-slim-bullseye",
+        self_repo_image="python_self:3.10.14-slim-bullseye",
+    )
+    trigger_res = action_trigger.create_workflow_dispatch_event(
+        "leowzz", "docker_image_pusher", api_workflow_id, trigger_args=trigger_args
+    )
     logger.info(f"{trigger_res=}")
-# response = requests.post(url, headers=headers, json=data)
-# response = requests.get(url, headers=headers)
-# print(response.status_code)
-# print(response.text)
-# if response.status_code == 204:
-#     print("Request was successful.")
-# else:
-#     print(f"Request failed with status code: {response.status_code}")
