04b0d780a5ec3ace946d240981c6bb8f1a74d076
WangZhanze <wangzhanze@huoban.ai>
2024-11-22 12:05:02 +0800

    format

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings
 
 BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
-print(f"{BASE_DIR=}")
+
 
 class Settings(BaseSettings):
     token: str  # 你的访问令牌 https://docs.github.com/zh/rest/actions/workflows?apiVersion=2022-11-28
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     https_proxy: str | None = None
     github_username: str = "leowzz"  # github用户名
     github_repo: str = "docker_image_pusher"  # github仓库名, fork此项目后的仓库名
-    
+
     class Config:
         env_file = os.path.join(BASE_DIR, ".env")
 
